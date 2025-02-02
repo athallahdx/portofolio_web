118feabf33dbe031d6e39d38ca0300ef66a2102b
athallahdx <athallahgamer4@gmail.com>
2025-02-02 14:17:53 +0700

    res

--- src/landingPage/Profile.jsx
+++ src/landingPage/Profile.jsx
@@ -75,13 +75,13 @@ export default function Profile() {
       {/* Profile Image */}
       <div className="
         flex justify-center items-center animate-bounceCustom 
-        mt-[50px] xs:mt-6 sm:mt-[0px] mb-0 xs:mb-0 md:mb-0 mdp:mt-0 
+        mt-[0px] xs:mt-6 sm:mt-[0px] mb-0 xs:mb-0 md:mb-0 mdp:mt-0 
         max-w-xs sm:max-w-lg mdp:max-w-xl lg:max-w-xl xl:max-w-2xl
       ">
         <img
           className="
             object-contain drop-shadow-2xl h-auto lg:w-auto 
-            max-w-[45vw] xs:max-w-[60vw] sm:max-w-[40vw] mdp:max-w-[50vw] 
+            max-w-[60vw] xs:max-w-[70vw] sm:max-w-[40vw] mdp:max-w-[50vw] 
             lg:max-h-[600px] xl:max-w-[30vw] 2xl:max-h-full xl:me-16 2xl:me-20
           "
           src={profil}
